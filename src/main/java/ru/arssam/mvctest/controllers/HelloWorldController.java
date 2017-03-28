1ef410c6743f15bd181ef5b3ebeb97da2e4b77ff
arseniy <arssam@mail.ru>
2017-03-28 20:13:30 +0300

    index.jsp location OK

--- src/main/java/ru/arssam/mvctest/controllers/HelloWorldController.java
+++ src/main/java/ru/arssam/mvctest/controllers/HelloWorldController.java
@@ -3,7 +3,6 @@ package ru.arssam.mvctest.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloWorldController{
