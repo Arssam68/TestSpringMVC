3c7a44417605e3b088b6a4d3633d4b6f671df53b
Arseniy <arssam@mail.ru>
2017-03-28 10:26:45 +0400

    initial commit

---
+++ src/main/java/ru/arssam/mvctest/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package ru.arssam.mvctest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorldController{
+    @RequestMapping(value = "/")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "index";
+    }
+}
