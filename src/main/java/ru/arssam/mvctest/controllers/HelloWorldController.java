c91bd0cd5eb34dce9aa31b5c68e0e9780631cfcf
arseniy <arssam@mail.ru>
2017-03-28 21:14:51 +0300

    addUsers!

--- src/main/java/ru/arssam/mvctest/controllers/HelloWorldController.java
+++ src/main/java/ru/arssam/mvctest/controllers/HelloWorldController.java
@@ -1,14 +1,33 @@
 package ru.arssam.mvctest.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import ru.arssam.mvctest.User;
 
 @Controller
 public class HelloWorldController{
     @RequestMapping(value = "/")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello World!");
+    public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ModelAndView user() {
+        return new ModelAndView("/user", "command", new User());
+    }
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public String addStudent(@ModelAttribute("mvc-dispatcher") User user,
+                             ModelMap model) {
+        model.addAttribute("id", user.getId());
+        model.addAttribute("name", user.getName());
+        model.addAttribute("speciality", user.getSpecialty());
+        model.addAttribute("experience", user.getExperience());
+
+        return "result";
     }
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,7 +1,10 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
+<head>
+    <title>Home Page</title>
+</head>
 <body>
-<h1>Spring MVC Hello World Example</h1>
-
+<h3><a href="/user">Add User</a></h3>
 </body>
 </html>
