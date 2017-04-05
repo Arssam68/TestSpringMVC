f044ffdeb7b7ed3b2d6bbc6df690ce405c62fb69
arseniy <arssam@mail.ru>
2017-04-05 21:03:38 +0300

    add logger

--- src/main/java/ru/arssam/mvctest/dao/UserDaoImpl.java
+++ src/main/java/ru/arssam/mvctest/dao/UserDaoImpl.java
@@ -3,12 +3,16 @@ package ru.arssam.mvctest.dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.arssam.mvctest.model.User;
 
 import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao {
+    private static final Logger logger = LoggerFactory.getLogger(UserDaoImpl.class);
+
     private SessionFactory sessionFactory;
 
     public void setSessionFactory(SessionFactory sessionFactory) {
@@ -19,12 +23,14 @@ public class UserDaoImpl implements UserDao {
     public void addUser(User user) {
         Session session = sessionFactory.getCurrentSession();
         session.persist(user);
+        logger.info("User successfully saved. User details: " + user);
     }
 
     @Override
     public void updateUser(User user) {
         Session session = sessionFactory.getCurrentSession();
         session.update(user);
+        logger.info("User successfully update. User details: " + user);
     }
 
     @Override
@@ -35,12 +41,14 @@ public class UserDaoImpl implements UserDao {
         if(user != null){
             session.delete(user);
         }
+        logger.info("User successfully removed. User details: " + user);
     }
 
     @Override
     public User getUserById(int id) {
         Session session = sessionFactory.getCurrentSession();
         User user = session.load(User.class, new Integer(id));
+        logger.info("User successfully loaded. User details: " + user);
         return user;
     }
 
@@ -48,6 +56,11 @@ public class UserDaoImpl implements UserDao {
     public List<User> listUsers() {
         Session session = sessionFactory.getCurrentSession();
         List<User> userList = session.createQuery("from User").list();
+
+        for(User user: userList){
+            logger.info("User list: " + user);
+        }
+
         return userList;
     }
 }
